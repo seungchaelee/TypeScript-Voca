0c0c622e52a8ae8aeda7f96579035ff064d999fa
seungchae <id6639@naver.com>
2022-06-13 20:45:54 +0900

    change title

--- src/component/Header.tsx
+++ src/component/Header.tsx
@@ -5,14 +5,14 @@ export default function Header() {
     <>
       <div className="header">
         <h1>
-          <Link to="/">토익 영단어(고급)</Link>
+          <Link to="/">TOEIC (고급)</Link>
         </h1>
         <div className="menu">
           <Link to="/create_word" className="link">
-            단어 추가
+            Add Word
           </Link>
           <Link to="/create_day" className="link">
-            day 추가
+            Add Day
           </Link>
         </div>
       </div>
