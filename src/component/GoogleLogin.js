dd8e110c9527279d8446bc0a46c153f3bf374877
seungchae <id6639@naver.com>
2023-05-23 21:00:14 +0900

    change login

--- src/component/GoogleLogin.js
+++ src/component/GoogleLogin.js
@@ -7,19 +7,24 @@ const GoogleLoginButton = () => {
 
   return (
     <>
-      <GoogleOAuthProvider clientId={clientId}>
+      <GoogleOAuthProvider clientId={`${clientId}`}>
         <GoogleLogin
-          buttonText='google logIn'
-          onSuccess={(res) => {
-            console.log(res);
-          }}
-          onFailure={(err) => {
-            console.log(err);
-          }}
+          clientId={`${clientId}`}
+          onSuccess={(res) => console.log(res, '성공')}
+          onFailure={(res) => console.log(res, '실패')}
+          render={(renderProps) => (
+            <div className='social_login_box google' onClick={renderProps.onClick}>
+              <div className='social_login_image_box'>
+                {/* <img src={googleIcon} alt='google_login' /> */}
+              </div>
+              <div className='social_login_text_box'>구글로 시작하기</div>
+              <div className='social_login_blank_box'> </div>
+            </div>
+          )}
         />
       </GoogleOAuthProvider>
     </>
   );
 };
 
-export default GoogleLoginButton
+export default GoogleLoginButton;
