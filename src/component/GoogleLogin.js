176c64d95b607965287a42f0990817eaca7a305d
seungchae <id6639@naver.com>
2023-05-24 13:30:42 +0900

    del google

--- src/component/GoogleLogin.js
+++ src/component/GoogleLogin.js
@@ -7,17 +7,16 @@ const GoogleLoginButton = () => {
 
   return (
     <>
-      <GoogleOAuthProvider clientId={`${clientId}`}>
+      <GoogleOAuthProvider clientId={clientId}>
         <GoogleLogin
-          clientId={`${clientId}`}
           onSuccess={(res) => console.log(res, '성공')}
           onFailure={(res) => console.log(res, '실패')}
-          render={(renderProps) => (
-            <div className='social_login_box google' onClick={renderProps.onClick}>
-              <div className='social_login_text_box'>구글로 시작하기</div>
-              <div className='social_login_blank_box'> </div>
-            </div>
-          )}
+          // render={(renderProps) => (
+          //   <div className='social_login_box google' onClick={renderProps.onClick}>
+          //     <div className='social_login_text_box'>구글로 시작하기</div>
+          //     <div className='social_login_blank_box'> </div>
+          //   </div>
+          // )}
         />
       </GoogleOAuthProvider>
     </>
